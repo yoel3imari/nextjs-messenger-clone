c5a782bd2f5c35397e2c31281ffaed3c257a7355
yoel3imari <youssef.elaimari@gmail.com>
2024-03-08 11:29:27 +0100

    fix webpack errors

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,7 +1,7 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { pusherServer } from "@/app/libs/pusher";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
+import { pusherServer } from "@/app/libs/pusher";
+import { authOptions } from "../../../app/api/auth/[...nextauth]/route";
 
 export default async function handler(
   req: NextApiRequest,
